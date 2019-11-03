8ec26b97de8ad1f6fb66aecbb1a992e60c2ad984
Eladio Rocha Vizcaino <eladio.rocha99@gmail.com>
2019-11-03 10:06:47 -0600

    Simplified files and optimized dist/utilities

---
+++ public/js/dist/google-maps-autocomplete.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let autocomplete = [],
+        waypoints = [[],[]],
+        inputIds = ['searchboxinput-origin', 'searchboxinput-destiny'];
+    for(let i = 0; i < 2; i++) {
+        autocomplete[i] = new google.maps.places.Autocomplete((document.getElementById(inputIds[i])), {
+            types: ['geocode'],
+            componentRestrictions: {
+              country: "MX",
+            }
+        });
+
+        google.maps.event.addListener(autocomplete[i], 'place_changed', function () {
+            let near_place = autocomplete[i].getPlace();
+            waypoints[i] = [near_place.geometry.location.lat(),near_place.geometry.location.lng()]
+            map.setView([near_place.geometry.location.lat(),near_place.geometry.location.lng()], 13)
+            if(waypoints[0].length === 2 && waypoints[1].length === 2) {
+                // The variable routeControl is in the file and too the variable map map.js
+                if(!!routeControl) {
+                    routeControl.getPlan().setWaypoints({latLng: L.latLng([0, 0])})
+                }
+                routeControl = L.Routing.control({
+                    waypoints: [
+                        L.latLng(waypoints[0][0], waypoints[0][1]),
+                        L.latLng(waypoints[1][0], waypoints[1][1])
+                    ],
+                    draggableWaypoints : false,//to set draggable option to false
+                    addWaypoints : false //disable adding new waypoints to the existing path 
+                }).addTo(map);
+            }
+        });
+    }
+
+
+    console.log(map)
+
+	
+
+
+    // google.maps.event.addListener(autocompleteDestiny, 'place_changed', function () {
+    //     let near_place = autocompleteDestiny.getPlace();
+    //     destiny = [near_place.geometry.location.lat(),near_place.geometry.location.lng()]
+    //     map.setView([near_place.geometry.location.lat(),near_place.geometry.location.lng()], 13)
+
+ 
+    // });
+
+
+
+});
--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,9 @@
+let map, routeControl
+
+// This file include only instances of map and datepicker
 (() => {
     //Inicializamos el mapa y le pasamos el id del contenedor html
-    let map = L.map('map').setView([51.505, -0.09], 13);
-
+    map = L.map('map').setView([34.04, -0.12], 13);
     //Mandamos a llamar el mapa y le pasamos una configuración por defecto, incluyendo el token
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxhZGlvcm9jaGEiLCJhIjoiY2syOTd6NjlhMThtMDNncWhjb3FvazBicyJ9.7zHOqJy7Oc4yOnFrXqDi1Q', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -27,17 +29,14 @@
         }
         alert(searchkeywords);
     }
+
+    //With this work the input
+    
     map.addControl(control);
-
-    //Buscador de lugares
-    let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-
-    autocomplete(countries)
-    collapsible()
-
+    
     // Air picker calendar settings
     // Initialization
-    let datepicker = $('#datepicker').datepicker({
+    $('#datepicker').datepicker({
         language: 'es',
         minDate: new Date(),
         timepicker: true,
@@ -46,6 +45,8 @@
         maxHours: 23,
         minutesStep: 5
     }).data('datepicker')
+
+    collapsible()
 
     // Load data from localstorage and show information
     let user = JSON.parse(localStorage.getItem('user'))
